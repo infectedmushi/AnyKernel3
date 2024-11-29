cc9a781516074f6b3173087592097bbdcfd81228
James McConnell <bins4us@hotmail.com>
2024-11-29 01:49:08 -0500

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -2,25 +2,52 @@
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
-# begin properties
+# global properties
 properties() { '
-kernel.string=GKI Pixel 8/Pro With KernelSU
-do.devicecheck=1
+kernel.string=KernelSU With SUSFS by TheWildJames
+do.devicecheck=0
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=husky
-device.name2=shiba
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
 supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
 '; } # end properties
 
-# boot image installation
+
+### AnyKernel install
+## boot shell variables
 block=boot
 is_slot_device=auto
 ramdisk_compression=auto
 patch_vbmeta_flag=auto
 no_magisk_check=1
-. tools/ak3-core.sh;
-split_boot;
-flash_boot;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh
+
+kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
+case $kernel_version in
+    5.1*) ksu_supported=true ;;
+    6.1*) ksu_supported=true ;;
+    *) ksu_supported=false ;;
+esac
+
+ui_print " " "  -> ksu_supported: $ksu_supported"
+$ksu_supported || abort "  -> Non-GKI device, abort."
+
+# boot install
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
+    split_boot # for devices with init_boot ramdisk
+    flash_boot # for devices with init_boot ramdisk
+else
+    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+fi
+## end boot install
